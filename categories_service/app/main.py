455821943fc5800eb1a50895ca396bd0fccdffba
Misha Osipov <mishaosipv@gmail.com>
2026-01-07 20:46:48 +0300

    feat: add RabbitMQ worker for category service

--- categories_service/app/main.py
+++ categories_service/app/main.py
@@ -1,18 +1,24 @@
+import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from app.api.routers import categories
 from app.core.database import create_db_and_tables
-
+from app.core.rabbitmq_worker import run_consumer
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Приложение запускается.")
+    # Запускаем consumer как фоновую задачу
+    consumer_task = asyncio.create_task(run_consumer())
     await create_db_and_tables()
-    print("БД инициализирована.")
+    print("Инициализация завершена.")
     yield
-    print("Приложение завершает работу.")
+    consumer_task.cancel()
+    try:
+        await consumer_task
+    except asyncio.CancelledError:
+        print("Consumer RabbitMQ остановлен.")
 
 app = FastAPI(
     title="Сервис для категорий",
